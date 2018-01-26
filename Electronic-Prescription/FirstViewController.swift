fea7f0d6001030613b393b04971d2e054f98a76d
Marco <rossnmarco@gmail.com>
2018-01-26 18:02:17 -0500

    Initial Commit

---
+++ Electronic-Prescription/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Electronic-Prescription
+//
+//  Created by Marco Ross on 2018-01-26.
+//  Copyright © 2018 rossmarco. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Electronic-Prescription/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Electronic-Prescription
+//
+//  Created by Marco Ross on 2018-01-26.
+//  Copyright © 2018 rossmarco. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
